1bdf6dd110ac04ce1aecb8e9c286597e0fe55676
rfdavid <ruifelipe@gmail.com>
2024-06-08 10:35:08 -0400

    use log2(capacity) for segment size

--- pma.cpp
+++ pma.cpp
@@ -1,3 +1,7 @@
+/* PMA Implementation 
+ * - no index
+ * - rebalance on each insert
+ */
 #include <iostream>
 #include <vector>
 #include <optional>
@@ -22,10 +26,20 @@ public:
     uint64_t capacity;
     uint64_t totalElements = 0;
 
+    // lower threshold at level 1
+    static constexpr double p1 = 0.5;
+    // upper threshold at level 1
+    static constexpr double t1 = 1;
+    // lower threshold at top level h
+    static constexpr double ph = 0.75;
+    // upper threshold at top level h
+    static constexpr double th = 0.75;
+
     PackedMemoryArray(uint64_t segmentSize) : segmentSize(segmentSize), capacity(segmentSize) {
-        data = std::vector<std::optional<int>>(segmentSize, std::nullopt);
+        data = std::vector<std::optional<int>>(log2(segmentSize), std::nullopt);
     }
 
+    /* Helper functions */
     void checkIfSorted() {
         for (uint64_t i = 0; i < capacity - 1; i++) {
             if (data[i] && data[i + 1] && data[i].value() > data[i + 1].value()) {
@@ -36,7 +50,7 @@ public:
         std::cout << "Array is sorted" << std::endl;
     }
 
-    void print(bool overwrite = false, uint64_t highlightNumber = UINT64_MAX) {
+    void print(bool overwrite = false, int highlightNumber = -1) {
         if (overwrite) {
             std::cout << "\033[2J\033[1;1H";
         }
@@ -55,15 +69,6 @@ public:
         std::cout << std::endl << std::endl;
     }
 
-    // lower threshold at level 1
-    static constexpr double p1 = 0.5;
-    // upper threshold at level 1
-    static constexpr double t1 = 1;
-    // lower threshold at top level h
-    static constexpr double ph = 0.75;
-    // upper threshold at top level h
-    static constexpr double th = 0.75;
-
     double upperThresholdAtLevel(int level) {
         int height = getTreeHeight();
         return th + (t1 - th) * (height - level) / (height - 1);
@@ -74,11 +79,10 @@ public:
         return ph - (ph - p1) * (height - level) / (height - 1);
     }
 
-
     void rebalance(uint64_t left, uint64_t right) {
         // calculate gaps and elements for that segment
         // temporarily store the elements in a vector
-        int numElements = 0;
+        uint64_t numElements = 0;
         int segmentSize = right - left;
         std::vector<int> segmentElements;
         segmentElements.reserve(segmentSize);
@@ -133,10 +137,7 @@ public:
                 right = mid - 1;
             } else {
                 // data[mid] is a gap (std::nullopt), search nearest non-nullopt values
-                int64_t nearestLeft = mid,  nearestRight = mid;
-
-                // 2 3 5 6 10 11 15 44 77 1554 1766 3151 4547 _  _  _  _  _  _  _   _  _  _  _
-                // 0 1 2 3 4  5  6  7  8  9    10   11   12   13 14 15 16 17 18 19 20 21 22 23
+                uint64_t nearestLeft = mid,  nearestRight = mid;
 
                 while (nearestLeft > left && !data[nearestLeft]) nearestLeft--;
                 while (nearestRight < right && !data[nearestRight]) nearestRight++;
@@ -192,7 +193,7 @@ public:
 
     double getDensity(uint64_t left, uint64_t right) {
         // if the segment is the root level, we don't
-        // have to perform a full linear search
+        // need to perform a full linear search
         if (left == 0 && right == capacity) {
             return static_cast<double>(totalElements) / capacity;
         }
@@ -208,6 +209,7 @@ public:
     void doubleCapacity() {
         capacity *= 2;
         data.resize(capacity, std::nullopt);
+        segmentSize = log2(capacity);
     }
 
     // in paper example
@@ -218,7 +220,6 @@ public:
 
     // check if the segment is balanced, if not, rebalance and check the upper level
     // if its balanced, check the upper level. If upper level is not balanced, rebalance and check the upper level
-    // 
     void checkForRebalancing(int index) {
         uint64_t segmentLeft = 0;
         uint64_t segmentRight = 0;
@@ -252,6 +253,7 @@ public:
                 DEBUG_PRINT << "Segment is not balanced" << std::endl;
                 // if level is root, double capacity and do full rebalancing
                 if (level == treeHeight) {
+                    DEBUG_PRINT << "Root level is unbalanced" << std::endl;
                     doubleCapacity();
                     rebalance(0, capacity - 1);
                 } else {
@@ -285,7 +287,6 @@ public:
                     gapIndex = leftCursor;
                     break;
                 }
-                // TODO: improve this, perhaps use int64_t
                 if (leftCursor == 0) continue;
                 leftCursor--;
             }
@@ -295,8 +296,6 @@ public:
 
     void insert(int value) {
         if (capacity == 0) return;
-        bool fullRebalance = false;
-        bool partialRebalance = false;
 
         uint64_t mid = binarySearchPMA(value);
         DEBUG_PRINT << "Value to insert: " << value << ", desired position: " << mid << std::endl;
@@ -310,6 +309,7 @@ public:
         if (data[mid] == std::nullopt) {
             DEBUG_PRINT << "Inserting value: " << value << " at index: " << mid << std::endl;
             insertElement(value, mid);
+            checkForRebalancing(mid);
             return;
         } 
 
@@ -348,28 +348,6 @@ public:
         // insert the value into mid position
         insertElement(value, mid);
         checkForRebalancing(mid);
-
-        // rebalance
-        // v1
-        // find the neareast gap, regardless the segment
-        // trigger rebalance
-        // that's it
-	// v2
-	// assume there will be always a gap in the segment:
-	// 1. shift left or right
-	// 2. insert
-	// 3. check if the segment is full
-	//    3a. is it full? rebalance the window (upper level)
-	//        upper level is not balanced? 
-	//           go to a higher level, rebalance
-	//              upper level is not balanced?
-	//                 go to a higher level, rebalance
-	//                 ...
-	//                 is root level? double the capacity and rebalance
-        //                 for root level, keep count the number of elements
-        //                 so we don't have to do a complete linear search
-	
-
     }
 };
 
@@ -377,87 +355,33 @@ void distInsert(PackedMemoryArray& pma) {
     Timer t;
 
     std::random_device rd;
-    std::mt19937 eng(-1011927998);
+    //std::mt19937 eng(-1011927998);
     int seed = rd();
 
     DEBUG_PRINT << "Seed: " << seed << std::endl;
-    //std::mt19937 eng(seed);
+    std::mt19937 eng(seed);
     std::uniform_int_distribution<> distr(0, 10000);
-
     t.start();
-    for (int count = 0; count < 1000000; count++) {
-//        int num = distr(eng);
-//        pma.insert(num);
-        pma.insert(count);
+
+    for (int count = 0; count < 1000; count++) {
+        int num = distr(eng);
+        pma.insert(num);
+        pma.print();
+//        pma.insert(count);
 //        pma.print(true, count);
 //        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
- //       pma.print();
 //        pma.checkIfSorted();
         
     }
 
     double time_taken = t.stop();
     std::cout << "Head Inserts: " << time_taken/10000000.0 << std::endl;
-    
 }
 
 int main() {
-    PackedMemoryArray pma(6);
-
-//    pma.insert(5);
-//    pma.print();
-//    pma.insert(15);
-//    pma.print();
-//    pma.insert(10);
-//    pma.print();
-//    pma.insert(2);
-//    pma.print();
-//    pma.insert(3);
-//    pma.print();
-//    pma.insert(11);
-//    pma.print();
-//    pma.insert(44);
-//    pma.print();
-//    pma.insert(6);
-//    pma.print();
-//    pma.insert(77);
-//    pma.print();
+    PackedMemoryArray pma(4 /* initial capacity */);
     distInsert(pma);
-
-    // pma.rebalance(0, pma.capacity - 1);
-    //pma.rebalance(0, 24);
- //   pma.print();
-
- //   pma.checkIfSorted();
- //   pma.printLevelsInformation();
+    pma.checkIfSorted();
 
     return 0;
 }
-
-
-// 2 3 5 6 10 11 15 44 77 1554 1766 3151 4547 _  _  _  _  _  _  _   _  _  _  _
-// 0 1 2 3 4  5  6  7  8  9    10   11   12   13 14 15 16 17 18 19 20 21 22 23
-
-//             x           x           x           x           x           x
-// 0 _ _ 1 _ _ _ 2 _ _ 3 _ _ _ 4 _ _ 5 _ _ _ 6 _ _ 7 _ _ _ 8 _ _ 9 _ _ _ 10 _ _ 11 _ _ _ 12 _ _ 13 _ _ 14 15 _ 16 17 _ 18 19 _ 20 21 _ 22 23 _ 24 25 _ 26 27 _ 28 29 _ 30 31 32 _ 33 34 _ 35 36 _ 37 38 _ 39 40 _ 41 42 43 44 45 46 47 48
-//
-        // count gaps and non-gaps
-        // [ _ _ 3 4 5 _ _ _ _ _ ]
-        // 6 gaps, 3 elements, 10 total
-        //
-        // gaps/elements = 2
-        // [ _ _ 3 _ _ 4 _ _ 5 _ ]
-        //
-        // [ _ _ 3 4 5 6 _ _ _ _ ]
-        // 6 gaps, 4 elements, 10 total
-        // 6/4 = 1.5
-        // [ 3 _ 4 _ 5 _ 6 _ _ _ ]
-        // [ 3 _ _ 4 _ _ 5 _ _ 6 ]
-        //
-        //
-        // [ 1 2 _ _ _ 3 4 5 6 7 ]
-        // 3 gaps, 6 elements, 10 total
-        // 0.5 gaps / element => 1 gap / 2 elements
-        // [ 1 2 _ 3 4 _ 5 6 _ 7 ]
-        //
-        //
